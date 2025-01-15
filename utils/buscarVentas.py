eabbb21de56641e670c79f4445793f0718432dec
Carlos Aranguren <arangurencg2@gmail.com>
2025-01-15 18:27:51 -0400

    [adicion]creacion de funcion para la obtencionde ventas segun el autoincrement

--- utils/buscarVentas.py
+++ utils/buscarVentas.py
@@ -20,9 +20,17 @@ async def buscarVentas(server: AsyncServer, time_to_stop: float):
             print(lastAuto)
             if lastAuto > int(autoIni):
                 with conn.cursor() as cursor:
-                    cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute())+ "/tmp/Ventas"}" FROM SOPERACIONINV 
+                    operaciones=cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute())+ "/tmp/Operaciones"}" FROM SOPERACIONINV 
                                         WHERE FTI_FECHAEMISION = '{str(datetime.date(datetime.now()))}' AND
-                                        FTI_AUTOINCREMENT > {autoIni} AND FTI_AUTOINCREMENT <= {lastAuto} """)
+                                        FTI_AUTOINCREMENT > {autoIni} AND FTI_AUTOINCREMENT <= {lastAuto} """).fetchall()
+                                         
+                    autos= tuple(x[0] for x in operaciones)
+                    print(autos)
+                    cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute())+ "/tmp/DetalleVenta"}" FROM SDETALLEVENTA 
+                                       WHERE FDI_OPERACION_AUTOINCREMENT IN {autos}""")
+                    
+                    cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute())+ "/tmp/DetalleCompra"}" FROM SDETALLECOMPRA
+                                       WHERE FDI_OPERACION_AUTOINCREMENT IN {autos}""")
                     #cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute()) + "/tmp/Ventas" }"  FROM SOPERACIONINV WHERE FTI_AUTOINCREMENT = {lastAuto}""")
                     #cursor.execute(f"""SELECT * INTO "{str(pathlib.Path().absolute()) + "/tmp/Detalle" }"  FROM SDETALLEVENTA WHERE FDI_OPERACION_AUTOINCREMENT = {lastAuto}""")
                 cursor.close()
@@ -33,11 +41,11 @@ async def buscarVentas(server: AsyncServer, time_to_stop: float):
 
 async def compactarTablas() :
     try:
-        zip_File = zipfile.ZipFile('ventasF.zip', 'w')
+        zip_File = zipfile.ZipFile('OperacionesF.zip', 'w')
         result = next(os.walk(f'{str((pathlib.Path().absolute()))}/tmp'))[2]
         print(result)
         for file in result:
-            if file[:-4] in ['Ventas', 'Detalle'] and file.endswith(('.idx', '.dat', '.blb')):
+            if file[:-4] in ['Operaciones', 'DetalleVenta', 'DetalleCompra'] and file.endswith(('.idx', '.dat', '.blb')):
                 zip_File.write(filename='{path}{file_name}'.format(path=f'tmp\\', file_name=file), arcname=file, compress_type=zipfile.ZIP_DEFLATED)
 
     except Exception as e:
